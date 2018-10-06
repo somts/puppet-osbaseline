7dcd150981762fe57324ca763ee81cbd45e63d5c
Jon C. Meyer <jmeyer@ucsd.edu>
2018-10-06 10:58:52 -0700

    silently fail for non-Linux OSes and accounts::users

--- manifests/users.pp
+++ manifests/users.pp
@@ -8,5 +8,10 @@ class osbaseline::users(
   create_resources('user',$users)
   create_resources('group',$groups)
   create_resources('ssh_authorized_key',$ssh_authorized_keys)
-  create_resources('accounts::users',$accounts_users)
+
+  # Accounts only works on Linux. For details, see
+  # https://forge.puppet.com/puppetlabs/accounts
+  if $::kernel == 'Linux' {
+    create_resources('accounts::users',$accounts_users)
+  }
 }
